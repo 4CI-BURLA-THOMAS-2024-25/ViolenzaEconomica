75a9fe00d552af1f48caf9d1a64d5a3e2d1e776a
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2025-05-05 18:45:40 +0200

    aggiungo menu nelle pagine; imposto schema pagina violenza tra cittadini

--- script/cittadiniCittadini.js
+++ script/cittadiniCittadini.js
@@ -1,10 +1,6 @@
 function inizializzaPagina() {
     alert("Benvenuto! Questo sito parla di un tema importante: la violenza economica.");
-
-    
-    document.getElementById("p1").style.backgroundColor = "#eef6f9";
-    document.getElementById("p2").style.backgroundColor = "#eef6f9";
-    document.getElementById("p3").style.backgroundColor = "#eef6f9";
-    document.getElementById("p4").style.backgroundColor = "#eef6f9";
-    document.getElementById("p5").style.backgroundColor = "#eef6f9";
 }
+
+//chiamo funzione al caricamento della pagina
+inizializzaPagina();
