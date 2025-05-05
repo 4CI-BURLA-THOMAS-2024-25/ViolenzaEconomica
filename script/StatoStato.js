6b6ccff60f1a4a782bcb2de4a300a948ae2c0052
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2025-05-05 17:53:42 +0200

    scrivo script, sistemo pagina, aggiungo cose al foglio di stile css, aggiungo foto a pagina iniziale

---
+++ script/StatoStato.js
@@ -0,0 +1,42 @@
+//prelevo paragrafi
+const paragrafi = document.querySelectorAll(".paragrafo");
+
+// prelevo bottoni per espandere paragrafi
+const bottoniEspanditori = document.querySelectorAll(".bottoneEspandi");
+//associo ascoltatore
+for(let i = 0; i < bottoniEspanditori.length; i++){
+    //associo al singolo bottone la funzione, passando come parametro l'indice alla funzione callback
+    bottoniEspanditori[i].addEventListener("click", () => gestisciParagrafo(i));
+}
+
+//funzione che stabilisce se espandere o comprimere il paragrafo
+function gestisciParagrafo(i){
+    //esamino testo del bottone per capire se espandere o comprimere paragrafo
+    if(bottoniEspanditori[i].innerHTML == "Espandi"){
+        mostraParagrafo(i);
+    }else{
+        comprimiParagrafo(i);
+    }
+}
+
+//funzione che mostra i paragrafi
+function mostraParagrafo(i){
+    //prelevo relativo paragrafo (escludo il primo, mostrato)
+    const paragrafo = paragrafi[i + 1];
+    //rendo visibile paragrafo
+    paragrafo.style.display = "flex";
+
+    //cambio testo bottone
+    bottoniEspanditori[i].innerText = "Comprimi";
+}
+
+// funzione che comprime i paragrafi
+function comprimiParagrafo(i){
+    //prelevo relativo paragrafo (escludo il primo, mostrato)
+    const paragrafo = paragrafi[i + 1];
+    //nascondo paragrafo
+    paragrafo.style.display = "none";
+
+    //cambio testo bottone
+    bottoniEspanditori[i].innerText = "Espandi";
+}
