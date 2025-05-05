83d721922bd5a88d558157bce73f61d451630231
Wu-Xuan-4CI <20027@studenti.marconiverona.edu.it>
2025-05-05 14:10:23 +0200

    aggiungo la mia parte

---
+++ script/cittadiniCittadini.js
@@ -0,0 +1,10 @@
+function inizializzaPagina() {
+    alert("Benvenuto! Questo sito parla di un tema importante: la violenza economica.");
+
+    
+    document.getElementById("p1").style.backgroundColor = "#eef6f9";
+    document.getElementById("p2").style.backgroundColor = "#eef6f9";
+    document.getElementById("p3").style.backgroundColor = "#eef6f9";
+    document.getElementById("p4").style.backgroundColor = "#eef6f9";
+    document.getElementById("p5").style.backgroundColor = "#eef6f9";
+}
