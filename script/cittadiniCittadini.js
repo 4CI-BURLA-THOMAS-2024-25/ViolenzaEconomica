55fa1029029637f3742228082cde6c329f9c2b9e
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2025-05-07 18:53:46 +0200

    metto insieme pezzi

--- script/cittadiniCittadini.js
+++ script/cittadiniCittadini.js
@@ -1,6 +1,21 @@
-function inizializzaPagina() {
-    alert("Benvenuto! Questo sito parla di un tema importante: la violenza economica.");
+function mostraFonti() {
+    var fontiBox = document.getElementById("fontiBox");
+
+    if (fontiBox.style.display === "none" || fontiBox.style.display === "") {
+        fontiBox.style.display = "block";
+    } else {
+        fontiBox.style.display = "none";
+    }
 }
 
-//chiamo funzione al caricamento della pagina
-inizializzaPagina();
+function cambiaStile() {
+    var corpo = document.body;
+
+    if (corpo.style.backgroundColor === "black") {
+        corpo.style.backgroundColor = "white";
+        corpo.style.color = "black";
+    } else {
+        corpo.style.backgroundColor = "black";
+        corpo.style.color = "white";
+    }
+}
